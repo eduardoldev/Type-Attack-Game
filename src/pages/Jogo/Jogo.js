daf61dacb4cedd346dea78d02ee2ce8319b2a37d
httpsGabrielle <gabrielleso125@gmail.com>
2023-01-04 23:39:24 -0300

    interface game

---
+++ src/pages/Jogo/Jogo.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Jogo.css'
+function Jogo() {
+  return (
+    <>
+        <div className='containerGames'>
+            <div className='header'>
+                <h1>Logo</h1>
+                
+            </div>
+            <div className='chat'>
+                <h1>Container</h1>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default Jogo
